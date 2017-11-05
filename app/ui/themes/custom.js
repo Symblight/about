7dd09553af6321f2f9dafe72fa92f5609743b67c
Aleksey <symblight@gmail.com>
2017-11-06 00:22:53 +0300

    Add themes

--- app/ui/themes/custom.js
+++ app/ui/themes/custom.js
@@ -1,4 +1,6 @@
 const Pink = ['#F50057', '#FF4081', '#F8BBD0', '#FCE4EC'];
+const Purple = ['#9C27B0', '#D500F9', '#9C27B0', '#AB47BC', '#BA68C8', '#CE93D8'];
+const Blue = ['#2196F3', '#1E88E5', '#0D47A1', '#42A5F5', '#64B5F6', '#90CAF9'];
 
 const theme = {
   MainTheme: {
